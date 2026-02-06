a89bf081a4ef6a7085d3b0b124e8810bc58c7b18
Michael Taylor <michaeltaylor@Michaels-MacBook-Air.local>
2026-02-06 13:30:50 -0600

    ci: Use stable Flutter version in build script

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# Install Flutter
-git clone https://github.com/flutter/flutter.git --depth 1
+# Install the latest stable version of Flutter
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:`pwd`/flutter/bin"
+
+# Run doctor to confirm Flutter installation
+flutter doctor
+
 # Run build
 flutter pub get
 flutter build web
